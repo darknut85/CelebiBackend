a824b46489fd2ac1fb4c270eab19448133493639
marco <marco.haasjes22@gmail.com>
2023-02-08 10:56:49 +0100

    added angular to project

---
+++ Celebi.Angular/ClientApp/src/components/pokedexRedBlue/pokedexRedBlue.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pokedexRedBlue-root',
+  templateUrl: './pokedexRedBlue.component.html',
+  styleUrls: ['./pokedexRedBlue.component.css']
+})
+export class PokedexRedBlueComponent {
+  title = 'celebi';
+  
+}
