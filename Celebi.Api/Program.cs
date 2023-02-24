fd26a97cf68968592dc59e688f0fd203187040d4
marco <marco.haasjes22@gmail.com>
2023-02-24 13:50:46 +0100

    restored all up to connectionstring in api

--- Celebi.Api/Program.cs
+++ Celebi.Api/Program.cs
@@ -15,9 +15,8 @@ namespace Celebi.Api
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<DataContext>
-                (options => { options.UseNpgsql(@"Host=localhost;Username=postgres;Port=1700;Password=Soraheliatos2@;Database=pokemon"); }, ServiceLifetime.Transient);
+                (options => { options.UseNpgsql(builder.Configuration.GetConnectionString("conn1")); }, ServiceLifetime.Transient);
             builder.Services.AddScoped<IPokemonService, PokemonService>();
-                             //AddScoped<IRepository, Repository>();
 
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
