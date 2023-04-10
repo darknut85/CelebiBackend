673d823bdc12562655b55908c9ed5192b66641a0
marco <marco.haasjes22@gmail.com>
2023-04-10 10:46:31 +0200

    connectionstring console obtained from appsettings

--- CelebiBackend/Program.cs
+++ CelebiBackend/Program.cs
@@ -18,18 +18,19 @@ namespace CelebiBackend
 
             //IConfigurationRoot root = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
 
+
+            Pokedex pokedex = new();
+
             ServiceCollection serviceProvider = new();
             IServiceProvider provider = serviceProvider.AddDbContext<DataContext>
                 (options => 
                 {
-                    options.UseNpgsql(@"Host=localhost;Username=postgres;Port=1700;Password=Soraheliatos2@;Database=pokemon"); 
+                    options.UseNpgsql(pokedex.Configurating()); 
                 }, ServiceLifetime.Transient)
                 .AddScoped<IPokemonService, PokemonService>()
                 .BuildServiceProvider();
 
             var pokemonService = provider.GetRequiredService<IPokemonService>();
-
-            Pokedex pokedex = new();
             pokedex.MainMenu(pokemonService);
         }
     }
@@ -70,6 +71,16 @@ namespace CelebiBackend
                 }
                 Console.Clear();
             }
+        }
+
+        public string Configurating()
+        {
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            //.AddEnvironmentVariables();
+            var config = builder.Build();
+
+            return config.GetConnectionString("PostgreSqlConnection");
         }
     }
 }
