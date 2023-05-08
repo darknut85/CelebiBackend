eff3e82518d03ac43fac8e2eaa48b50f03cf37cd
marco <marco.haasjes22@gmail.com>
2023-05-08 13:38:17 +0200

    changed location background

--- celebi/src/app/components/front2/front2.component.ts
+++ celebi/src/app/components/front2/front2.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-front2',
   templateUrl: './front2.component.html',
   styleUrls: ['./front2.component.css']
 })
-export class Front2Component implements OnInit {
-  constructor(){}
-  
-  ngOnInit(): void {}
-
-  rotation(): void
-  {
-  }
-}
+export class Front2Component{}
