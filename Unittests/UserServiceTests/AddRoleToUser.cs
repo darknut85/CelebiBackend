19f2a42ed995daaf54d402af62167c755aded3fa
marco <marco.haasjes22@gmail.com>
2023-06-12 13:16:39 +0200

    addRoleToUser is tested

--- Unittests/UserServiceTests/AddRoleToUser.cs
+++ Unittests/UserServiceTests/AddRoleToUser.cs
@@ -1,12 +1,139 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Migrations;
+using Moq;
+using Objects;
+using Services;
+using Test.Helpers;
+using Xunit;
 
 namespace Unittests.UserServiceTests
 {
-    internal class AddRoleToUser
+    public class AddRoleToUser : IDisposable
     {
+        readonly UserService userService;
+        readonly DbContextOptions<DataContext> options;
+        private readonly DataContext context;
+        private readonly IConfiguration _configuration;
+        private readonly Mock<RoleManager<IdentityRole>> roleManagerMock;
+        private readonly Mock<UserManager<IdentityUser>> userManagerMock;
+        private readonly IdentityUser identityUser;
+        public AddRoleToUser() 
+        {
+            userManagerMock = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+            roleManagerMock = new Mock<RoleManager<IdentityRole>>(Mock.Of<IRoleStore<IdentityRole>>(), null, null, null, null);
+
+            options = this.CreatePostgreSqlUniqueClassOptions<DataContext>();
+            context = new(options);
+            context.DefaultSetup();
+
+            userService = new UserService(context, userManagerMock.Object, roleManagerMock.Object, _configuration);
+
+            identityUser = new IdentityUser()
+            {
+                Id = "125",
+                UserName = "Juan",
+                PasswordHash = "AQAAAAEAACcQAAAAEDwVLvLsPe2ydTBJ4DS5w+fMM9MX5pzjNRvjo/105TDE2LMp8rxKsrAAwc4Dh/yQFg==",
+                Email = "new.juan@newJuan.com",
+                NormalizedEmail = "NEW.JUAN@NEWJUAN.COM",
+                NormalizedUserName = "NEWJUAN",
+                EmailConfirmed = true
+            };
+        }
+
+        [Fact]
+        public async Task Get_Should_AddRole()
+        {
+            //arrange
+            UserCredential userCredential = new UserCredential() { Id = 1, Password = "", UserName = "" };
+            IList<string> role = new List<string>() { "Admin" };
+            IQueryable<IdentityRole> roleQueries = new List<IdentityRole>() {
+                new IdentityRole()
+                {
+                    Id = "457",
+                    Name = "User",
+                    NormalizedName = "USER"
+
+                }
+            }.AsQueryable();
+            userManagerMock.Setup(r => r.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(identityUser));
+            userManagerMock.Setup(r => r.GetRolesAsync(identityUser)).Returns(Task.FromResult(role));
+            roleManagerMock.Setup(r => r.Roles).Returns(roleQueries);
+            userManagerMock.Setup(r => r.AddToRoleAsync(identityUser, "")).ReturnsAsync(new IdentityResult() { });
+
+
+            //act
+            string isAdded = await userService.AddRoleToUser("User", "Juan");
+
+            //assert
+            Assert.True(isAdded == "The role has been added to the user");
+        }
+
+        [Fact]
+        public async Task Get_Should_NotAddRole_WhenUserDoesNotExist()
+        {
+            //arrange
+            UserCredential userCredential = new UserCredential() { Id = 1, Password = "", UserName = "" };
+            IList<string> role = new List<string>() { "Admin" };
+            IdentityUser noName = new() { UserName = ""};
+            userManagerMock.Setup(r => r.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(noName));
+
+            //act
+            string isAdded = await userService.AddRoleToUser("User", "Juan");
+
+            //assert
+            Assert.True(isAdded == "The user does not exist");
+        }
+
+        [Fact]
+        public async Task Get_Should_NotAddRole_WhenRoleDoesNotExist()
+        {
+            //arrange
+            UserCredential userCredential = new UserCredential() { Id = 1, Password = "", UserName = "" };
+            IList<string> role = new List<string>() { "Admin" };
+            IQueryable<IdentityRole> roleQueries = new List<IdentityRole>() {
+            }.AsQueryable();
+            userManagerMock.Setup(r => r.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(identityUser));
+            userManagerMock.Setup(r => r.GetRolesAsync(identityUser)).Returns(Task.FromResult(role));
+
+            //act
+            string isAdded = await userService.AddRoleToUser("User", "Juan");
+
+            //assert
+            Assert.True(isAdded == "The role does not exist");
+        }
+
+        [Fact]
+        public async Task Get_Should_NotAddRole_WhenUserAlreadyHasRole()
+        {
+            //arrange
+            UserCredential userCredential = new UserCredential() { Id = 1, Password = "", UserName = "" };
+            IList<string> role = new List<string>() { "Admin" };
+            IQueryable<IdentityRole> roleQueries = new List<IdentityRole>() {
+                new IdentityRole()
+                {
+                    Id ="997",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }
+            }.AsQueryable();
+            userManagerMock.Setup(r => r.FindByNameAsync(It.IsAny<string>())).Returns(Task.FromResult(identityUser));
+            userManagerMock.Setup(r => r.GetRolesAsync(identityUser)).Returns(Task.FromResult(role));
+            roleManagerMock.Setup(r => r.Roles).Returns(roleQueries);
+            userManagerMock.Setup(r => r.AddToRoleAsync(identityUser, "")).ReturnsAsync(new IdentityResult() { });
+
+
+            //act
+            string isAdded = await userService.AddRoleToUser("Admin", "Juan");
+
+            //assert
+            Assert.True(isAdded == "The user already as the role");
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+        }
     }
 }
