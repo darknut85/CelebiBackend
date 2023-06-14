0d53d7bc6182524543120b13878edb2e8cf5c62b
marco <marco.haasjes22@gmail.com>
2023-06-14 11:08:10 +0200

    user cannot see admin functionality

--- celebi/src/app/components/auth/authguard.service.ts
+++ celebi/src/app/components/auth/authguard.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import {CanActivate, Router, UrlTree} from '@angular/router';
 import { Observable } from "rxjs";
 import { AuthService } from './auth.service';
+import { AdminService } from '../admin/admin.service';
+import { Role } from 'src/app/objects/role';
 
 @Injectable()
 export class AuthGuard implements CanActivate 
 {
-    constructor(private authService: AuthService, private router: Router) { }
+    constructor(private authService: AuthService, private router: Router, private adminService: AdminService) { }
+    roles: Role[] = [];
+    r: Role = {name: ""};
+    isAdmin = false;
 
     canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> 
     {
@@ -15,6 +20,28 @@ export class AuthGuard implements CanActivate
         {
             this.router.navigate(['login']);
         }
+
+        let userName = this.adminService.displayLogin();
+        this.AuthorizedToView(userName);
+        if(!this.isAdmin)
+        {
+            this.router.navigate(['home']);
+        }
+
         return loggedIn;
     }
+
+    AuthorizedToView(userName: string) {
+        this.adminService.getRoles(String(userName)).subscribe((role: Role[]) => {
+          role.forEach(element => {
+            this.r = {name:element.toString()};
+            this.roles.push(this.r);
+          });
+          this.roles.forEach(x => {
+            if(x.name == "Admin"){
+              this.isAdmin = true;
+            }
+          });
+        });
+      }
 }
