8d10942eeebf9f010f24dc2a09e2c662b144598e
marco <marco.haasjes22@gmail.com>
2023-06-21 15:16:31 +0200

    added authguard that allows any loggedin user

--- celebi/src/app/components/users/auth/authguard.service.ts
+++ celebi/src/app/components/users/auth/authguard.service.ts
@@ -13,6 +13,17 @@ export class AuthGuard implements CanActivate
     r: Role = {name: ""};
     isAdmin = false;
 
+    userCanActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree>
+    {
+      const loggedIn = this.authService.isLoggedIn();
+      if(!loggedIn)
+      {
+          this.router.navigate(['login']);
+      }
+
+      return loggedIn;
+    }
+
     canActivate(): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> 
     {
         const loggedIn = this.authService.isLoggedIn();
