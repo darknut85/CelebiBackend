22495f2cc3ef52b02b66de7b496a575dd96b70b3
marco <marco.haasjes22@gmail.com>
2023-06-26 09:49:52 +0200

    added placeholders for different option in profile

--- celebi/src/app/components/users/profile/profile.component.ts
+++ celebi/src/app/components/users/profile/profile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin/admin.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  constructor(private adminService: AdminService){}
+
+  userName = "";
+
+  ngOnInit()
+  {
+    this.userName = this.adminService.displayLogin();
+  }
 }
