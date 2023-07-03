4f098115e88d794b4aa5b13d1a23b7ab879cd382
marco <marco.haasjes22@gmail.com>
2023-07-03 11:08:24 +0200

    changes sidenav when manually refreshed

--- celebi/src/app/app.component.ts
+++ celebi/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './components/users/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'celebi';
   loggedIn = "";
 
-  constructor() { }
+  turnOn = true;
+
+  constructor(private authService: AuthService) { }
+  ngOnInit(): void {
+    this.turnOn = this.authService.isLoggedIn();
+  }
 }
