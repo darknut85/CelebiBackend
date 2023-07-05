b360d3460d7d51e18e58565eb380c88f94bb4a64
marco <marco.haasjes22@gmail.com>
2023-07-05 15:01:00 +0200

    pokedex update now has all messages

--- celebi/src/app/components/pokedex/pokedex-update/pokedex-update.component.ts
+++ celebi/src/app/components/pokedex/pokedex-update/pokedex-update.component.ts
@@ -14,11 +14,11 @@ export class PokedexUpdateComponent
               public dialogRef: MatDialogRef<PokedexUpdateComponent>, 
               @Inject(MAT_DIALOG_DATA) public data: Pokemon){
   }
-  
   selectedPokemon = 'selectedPokemon';
   parray: Pokemon[] = [];
   pokemon: Pokemon = <Pokemon>{ };
-  currentPokemon = -1;
+  currentPokemon = "";
+  message = "";
 
   ngOnInit() {
     this.pokemonService.getPokemon().subscribe((data: Pokemon[]) => {
@@ -31,8 +31,84 @@ export class PokedexUpdateComponent
     if( pokemon != undefined)
     {
       this.pokemon = pokemon;
-      this.currentPokemon = pokemon.id;
+      this.currentPokemon = pokemon.name;
     }
+  }
+
+  nameMade()
+  {
+    this.currentPokemon = this.pokemon.name;
+  }
+
+  dataCheck()
+  {
+    this.message = "One or more fields must be a number";
+    if(isNaN(this.pokemon.dexEntry) || this.pokemon.dexEntry.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.height) || this.pokemon.height.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.weight) || this.pokemon.weight.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.growthRate) || this.pokemon.growthRate.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.hpev) || this.pokemon.hpev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.atkev) || this.pokemon.atkev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.defev) || this.pokemon.defev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spatkev) || this.pokemon.spatkev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spdefev) || this.pokemon.spdefev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spdev) || this.pokemon.spdev.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.hp) || this.pokemon.hp.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.atk) || this.pokemon.atk.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.def) || this.pokemon.def.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spatk) || this.pokemon.spatk.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spdev)  || this.pokemon.spdef.toString() == "")
+    {
+      return false;
+    }
+    if(isNaN(this.pokemon.spd)  || this.pokemon.spd.toString() == "")
+    {
+      return false;
+    }
+    this.message = "";
+    return true;
   }
 
   onNoClick(): void{
